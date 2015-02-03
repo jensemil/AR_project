7a720adfbf14423cda128a9ad5da02ed36e7c80c
Jens Emil <je@gronbak.dk>
2015-02-03 14:18:24 +0100

    add files

---
+++ core/src/dk/au/cs/CVMain.java
@@ -0,0 +1,33 @@
+package dk.au.cs;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class CVMain extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat m = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("m =" + m.dump());
+    }
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+}
