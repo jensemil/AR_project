991a5a62e6e32e4632a1fe68fc6e73ace5733f60
Jens Emil <je@gronbak.dk>
2015-02-03 15:12:59 +0100

    add camera capture

--- core/src/dk/au/cs/CVMain.java
+++ core/src/dk/au/cs/CVMain.java
@@ -8,26 +8,52 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.VideoCapture;
 
 public class CVMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+    VideoCapture camera;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat m = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("m =" + m.dump());
+
+        camera = new VideoCapture(0);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(!camera.isOpened()){
+            System.out.println("Camera Error");
+        }
+        else{
+            System.out.println("Camera OK?");
+        }
+
     }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+
+        Mat eye = Mat.eye(128, 128, CvType.CV_8UC1);
+
+        Core.multiply(eye, new Scalar(255), eye);
+
+        camera.read(eye);
+
+        UtilAR.imDrawBackground(eye);
+
 	}
+
+    @Override
+    public void dispose() {
+        camera.release();
+    }
 }
