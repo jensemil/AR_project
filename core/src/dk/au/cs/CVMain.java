6c2510e9ce46580e7adae8bae8916eef96bcafe7
MortenBirk <mortenhbirk@gmail.com>
2015-02-05 22:24:37 +0100

    Add Canny edge detection just proof of concept

--- core/src/dk/au/cs/CVMain.java
+++ core/src/dk/au/cs/CVMain.java
@@ -5,20 +5,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
 import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+
+import static org.opencv.imgproc.Imgproc.blur;
+import static org.opencv.imgproc.Imgproc.cvtColor;
 
 public class CVMain extends ApplicationAdapter {
 
     VideoCapture camera;
+    private Mat videoInput;
+    private Mat detectedEdges;
 
 	@Override
 	public void create () {
-
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        detectedEdges = Mat.eye(128, 128, CvType.CV_8UC1);
+        videoInput = Mat.eye(128, 128, CvType.CV_8UC1);
 
         camera = new VideoCapture(0);
         try {
@@ -30,10 +34,9 @@ public class CVMain extends ApplicationAdapter {
         if(!camera.isOpened()){
             System.out.println("Camera Error");
         }
-        else{
+        else {
             System.out.println("Camera OK?");
         }
-
     }
 
 	@Override
@@ -41,19 +44,20 @@ public class CVMain extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
-        Mat eye = Mat.eye(128, 128, CvType.CV_8UC1);
-
-        Core.multiply(eye, new Scalar(255), eye);
-
-        camera.read(eye);
-
-        UtilAR.imDrawBackground(eye);
-
+        camera.read(videoInput);
+        UtilAR.imDrawBackground(videoInput);
+        doCanny();
 	}
 
     @Override
     public void dispose() {
         camera.release();
+    }
+
+    private void doCanny() {
+        Imgproc.cvtColor(videoInput, detectedEdges, Imgproc.COLOR_RGB2GRAY);
+        blur(detectedEdges, detectedEdges, new Size(3,3));
+        Imgproc.Canny(detectedEdges, detectedEdges, 50,100);
+        UtilAR.imShow(detectedEdges);
     }
 }
