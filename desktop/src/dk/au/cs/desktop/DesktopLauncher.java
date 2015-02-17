702c43e08eab004b8987f4b1128a67db8d6c2a5a
Mathias Skovgaard Birk <msbirk@cs.au.dk>
2015-02-17 14:43:05 +0100

    Approximating and drawing polygons

--- desktop/src/dk/au/cs/desktop/DesktopLauncher.java
+++ desktop/src/dk/au/cs/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package dk.au.cs.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dk.au.cs.CVChessboard;
 import dk.au.cs.CVMain;
 
 public class DesktopLauncher {
