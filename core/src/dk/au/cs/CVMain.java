cdadaa142d93617eb9ba18ac5007bb08b46584dc
Jens Emil <je@gronbak.dk>
2015-03-03 13:03:35 +0100

    some changes

--- core/src/dk/au/cs/CVMain.java
+++ core/src/dk/au/cs/CVMain.java
@@ -245,6 +245,12 @@ public class CVMain extends ApplicationAdapter {
 
         findRectangles();
         handleRectangles();
+        checkForCollision();
+
+    }
+
+    private void checkForCollision() {
+
     }
 
     private void animateSquare() {
